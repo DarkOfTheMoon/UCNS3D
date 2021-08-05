9dae8f12d3d96b0ab70a61908cb1f1c5c14f5ca3
Dean Yuan <dean.yuan@cranfield.ac.uk>
2021-08-05 16:04:30 +0100

    Fixed DG_RHS_VOL_INTEGRAL test

--- CODE/unit_test.f90
+++ CODE/unit_test.f90
@@ -64,60 +64,99 @@ FUNCTION TEST_DG_SOL(N,NUM_VARIABLES,ORDER,NUM_DOFS)
 !> Tests the DG_SOL function which returns the DG solution at a given point using the generic polynomial approximation
     IMPLICIT NONE
     INTEGER,INTENT(IN)::N,ORDER,NUM_DOFS,NUM_VARIABLES
-    REAL::TEST_X,TEST_Y
+    REAL::TEST_X,TEST_Y,EXPECTED_ANS
     REAL,DIMENSION(NUM_VARIABLES)::DG_SOL_TEST
-    REAL,DIMENSION(NUM_DOFS+1)::TEST_U_C
-    INTEGER::TEST_DG_SOL
+    REAL,DIMENSION(NUM_VARIABLES,NUM_DOFS+1)::TEST_U_C
+    INTEGER::I_VAR,TEST_DG_SOL
         
     TEST_DG_SOL = 0
 
     TEST_X = 2
     TEST_Y = 3
+    EXPECTED_ANS = 139
     
-    TEST_U_C = (/ 2, 3, 4, 5, 6, 7 /)
+    DO I_VAR = 1, NUM_VARIABLES
+        TEST_U_C(I_VAR,:) = (/ 2, 3, 4, 5, 6, 7 /)
+    END DO
     
     DG_SOL_TEST = DG_SOL(N,TEST_X,TEST_Y,NUM_VARIABLES,ORDER,NUM_DOFS,TEST_U_C)
     
-    IF (DG_SOL_TEST(1) /= 139) THEN
-        WRITE(400+N,*) 'Expected value: 139, Returned value:', DG_SOL_TEST
+    IF (DG_SOL_TEST(1) /= EXPECTED_ANS) THEN
+        WRITE(400+N,*) 'Expected value:', EXPECTED_ANS, 'Returned value:', DG_SOL_TEST
         TEST_DG_SOL = 1
     END IF
 
 END FUNCTION TEST_DG_SOL
 
-FUNCTION TEST_DG_RHS_VOL_INTEGRAL(N, ORDER, NUM_DOFS)
+FUNCTION TEST_DG_RHS_INTEGRAL(N, NUM_VARS, ORDER, NUM_DOFS)
 !> @brief
-!> Tests the volume integral term of the DG RHS on a triangle (0,0)(1,0)(0,1) by comparing to the analytical integral result for generic polys. Tests for other element shapes need to be added
+!> Tests the integral terms of the DG RHS on a triangle (0,0)(1,0)(0,1) and a quad (0,0)(1,0)(0,1)(1,1) by comparing to the analytical integral result for generic polys. Tests for 3d element shapes, other basis polys should be added
     IMPLICIT NONE
-    INTEGER,INTENT(IN)::N,ORDER,NUM_DOFS
-    INTEGER::I_QP !Iterator for quadrature points
-    REAL::TEST_DG_RHS_VOL,CELL_VOLUME
-	REAL,DIMENSION(3)::WEIGHTS_TEMP_TRI !Quadrature weights for domains
-	REAL,DIMENSION(2,3)::QPOINTS_TRI !Quadrature points for domains
-    REAL,DIMENSION(6)::TEST_U_C !2nd order solution value and dofs
-    INTEGER::TEST_DG_RHS_VOL_INTEGRAL
+    INTEGER,INTENT(IN)::N, NUM_VARS, ORDER, NUM_DOFS
+    INTEGER::I_QP, I_DOFS, I_VAR, TEST_DG_RHS_INTEGRAL !Iterator for quadrature points, DoFs, vars
+    REAL::CELL_VOLUME
+    REAL,DIMENSION(NUM_VARS,NUM_DOFS+1)::TEST_DG_RHS_VOL, DG_RHS_VOL_ANALYTICAL, DG_RHS_VOL_DIFF, TEST_U_C
     
-    TEST_DG_RHS_VOL_INTEGRAL = 0
+    TEST_DG_RHS_INTEGRAL = 0
     
-!     CELL_VOLUME = 0.5 !Volume of a triangle (0,0),(0,1),(1,0)
-! 
-!     VEXT(1,:) = (/ 0,0 /); VEXT(2,:) = (/ 1,0 /); VEXT(3,:) = (/ 0,1 /)
-!     CALL QUADRATURETRIANGLE(N,MIN(ORDER,6))
-!     WEIGHTS_TEMP_TRI = WEQUA3D(1:QP_TRIANGLE)
-!     QPOINTS_TRI = QPOINTS(1:2,1:QP_TRIANGLE)
-! 
-!     TEST_U_C = (/ 2, 3, 4, 5, 6, 7 /)
-!     
-!     DO I_QP = 1, QP_TRIANGLE   
-!         TEST_DG_RHS_VOL = TEST_DG_RHS_VOL+ DG_RHS_VOL_INTEGRAL(N,QPOINTS_TRI(1,I_QP),QPOINTS_TRI(2,I_QP),WEIGHTS_TEMP_TRI(I_QP),ORDER,NUM_DOFS,CELL_VOLUME,TEST_U_C)
-!     END DO
-!     
-!     IF (TEST_DG_RHS_VOL - 14.4583333333334 > 1E-12) THEN    
-!         WRITE(400+N,*) "DG RHS VOL COMPUTED VALUE: ", TEST_DG_RHS_VOL, "EXPECTED VALUE:", 14.4583333333334
-!         TEST_DG_RHS_VOL_INTEGRAL = 1
-!     END IF
+    ! Triangle
+    CELL_VOLUME = 0.5 !Volume of a triangle (0,0),(0,1),(1,0)
 
-END FUNCTION TEST_DG_RHS_VOL_INTEGRAL
+    VEXT(1,:) = (/ 0,0 /); VEXT(2,:) = (/ 1,0 /); VEXT(3,:) = (/ 0,1 /)
+    CALL QUADRATURETRIANGLE(N,MIN(ORDER,6))
+    
+    DO I_VAR = 1, NUM_VARS
+        TEST_U_C(I_VAR,:) = (/ 2, 3, 4, 5, 6, 7 /)
+        DG_RHS_VOL_ANALYTICAL(I_VAR,:) = (/ 0., 41./12, 41./12, 73./30, 61./24, 53./20 /)
+    END DO
+    
+    TEST_DG_RHS_VOL = 0
+    DO I_QP = 1, QP_TRIANGLE ! QP_TRIANGLE is global
+        TEST_DG_RHS_VOL = TEST_DG_RHS_VOL + DG_RHS_INTEGRAL(N,QPOINTS(1,I_QP),QPOINTS(2,I_QP),WEQUA3D(I_QP),NUM_VARS,ORDER,NUM_DOFS,CELL_VOLUME, DG_SOL(N,QPOINTS(1,I_QP),QPOINTS(2,I_QP),NUM_VARS,ORDER,NUM_DOFS,TEST_U_C), 1)
+    END DO
+
+
+    DG_RHS_VOL_DIFF = ABS(TEST_DG_RHS_VOL - DG_RHS_VOL_ANALYTICAL)
+!     WRITE(400+N,*) 'DIFF:', DG_RHS_VOL_DIFF, '1:', TEST_DG_RHS_VOL, '2:', DG_RHS_VOL_ANALYTICAL
+        
+    DO I_VAR = 1, NUM_VARS
+        DO I_DOFS = 1, NUM_DOFS + 1
+            IF (DG_RHS_VOL_DIFF(I_VAR,I_DOFS) > 1E-12) THEN
+                WRITE(400+N,*) I_DOFS, "DG RHS VOL TRI COMPUTED VALUE: ", TEST_DG_RHS_VOL(I_VAR,I_DOFS), "EXPECTED VALUE:", DG_RHS_VOL_ANALYTICAL(I_VAR,I_DOFS)
+                TEST_DG_RHS_INTEGRAL = 1
+            END IF
+        END DO
+    END DO
+    
+    ! Quad
+    CELL_VOLUME = 1 !Volume of a quadrilateral (0,0),(0,1),(1,0),(1,1)
+
+    VEXT(1,:) = (/ 0,0 /); VEXT(2,:) = (/ 1,0 /); VEXT(3,:) = (/ 0,1 /); VEXT(4,:) = (/ 1,1 /)
+    CALL QUADRATUREQUAD(N,MIN(ORDER,6))
+    
+    DO I_VAR = 1, NUM_VARS
+        TEST_U_C(I_VAR,:) = (/ 2, 3, 4, 5, 6, 7 /)
+        DG_RHS_VOL_ANALYTICAL(I_VAR,:) = (/ 0., 11., 11., 77./6, 157./12, 40./3 /)
+    END DO
+        
+    TEST_DG_RHS_VOL = 0
+    DO I_QP = 1, QP_QUAD ! QP_QUAD is global
+        TEST_DG_RHS_VOL = TEST_DG_RHS_VOL + DG_RHS_INTEGRAL(N,QPOINTS(1,I_QP),QPOINTS(2,I_QP),WEQUA3D(I_QP),NUM_VARS,ORDER,NUM_DOFS,CELL_VOLUME, DG_SOL(N,QPOINTS(1,I_QP),QPOINTS(2,I_QP),NUM_VARS,ORDER,NUM_DOFS,TEST_U_C), 1)
+    END DO
+    
+    DG_RHS_VOL_DIFF = ABS(TEST_DG_RHS_VOL - DG_RHS_VOL_ANALYTICAL)
+!     WRITE(400+N,*) 'DIFF:', DG_RHS_VOL_DIFF, '1:', TEST_DG_RHS_VOL, '2:', DG_RHS_VOL_ANALYTICAL
+        
+    DO I_VAR = 1, NUM_VARS
+        DO I_DOFS = 1, NUM_DOFS + 1
+            IF (DG_RHS_VOL_DIFF(I_VAR,I_DOFS) > 1E-12) THEN
+                WRITE(400+N,*) I_DOFS, "DG RHS VOL QUAD COMPUTED VALUE: ", TEST_DG_RHS_VOL(I_VAR,I_DOFS), "EXPECTED VALUE:", DG_RHS_VOL_ANALYTICAL(I_VAR,I_DOFS)
+                TEST_DG_RHS_INTEGRAL = 1
+            END IF
+        END DO
+    END DO
+
+END FUNCTION TEST_DG_RHS_INTEGRAL
 
 FUNCTION TEST_QP_ARRAY(N)
 !> @brief
@@ -140,6 +179,7 @@ FUNCTION TEST_QP_ARRAY(N)
                 CALL QUADRATURETRIANGLE(N,IGQRULES)
                 NUM_QP = QP_LINE_N
             END IF
+            
             DO I_QP = 1, NUM_QP
                 IF (QP_ARRAY(I,I_QP)%X /= QPOINTS(1,I_QP)) THEN
                     WRITE(400+N,*) "QP_ARRAY(",I,",",I_QP,")%X RETURNED VALUE: ", QP_ARRAY(1,I_QP)%X, "EXPECTED VALUE: ", QP_ARRAY(1,I_QP)%X
@@ -159,34 +199,6 @@ FUNCTION TEST_QP_ARRAY(N)
 
 END FUNCTION TEST_QP_ARRAY
 
-FUNCTION TEST_DG_RHS_SURF_INTEGRAL(N,ORDER)
-    IMPLICIT NONE
-    INTEGER,INTENT(IN)::N,ORDER
-    INTEGER::TEST_DG_RHS_SURF_INTEGRAL
-    INTEGER::I_QP !Iterator for quadrature points
-    REAL::TEST_DG_RHS_ANS,CELL_SURF
-	REAL,DIMENSION(3)::WEIGHTS_TEMP_LINE !Quadrature weights for interfaces
-	REAL,DIMENSION(2,3)::QPOINTS_LINE!Quadrature points for interfaces
-    REAL,DIMENSION(6)::TEST_U_C !2nd order solution value and dofs
-    
-    TEST_DG_RHS_SURF_INTEGRAL = 0
-    
-!     CELL_SURF = 0 !Volume of a triangle (0,0),(0,1),(1,0)
-! 
-!     VEXT(1,:) = (/ 0,0 /); VEXT(2,:) = (/ 1,0 /); VEXT(3,:) = (/ 0,1 /)
-!     CALL QUADRATURELINE(N,MIN(ORDER,6))
-!     WEIGHTS_TEMP_LINE = WEQUA2D(1:QP_TRIANGLE)
-!     QPOINTS_LINE = QPOINTS2D(1:2,1:QP_LINE_N)
-! 
-!     TEST_U_C = (/ 2, 3, 4, 5, 6, 7 /)
-!     
-!     IF (TEST_DG_RHS_ANS - 14.4583333333334 > 1E-12) THEN    
-!         WRITE(400+N,*) "DG RHS SURFACE INTEGRAL COMPUTED VALUE: ", TEST_DG_RHS_ANS, "EXPECTED VALUE:", 14.4583333333334
-!         TEST_DG_RHS_SURF_INTEGRAL = 1
-!     END IF
-    
-END FUNCTION TEST_DG_RHS_SURF_INTEGRAL
-
 SUBROUTINE CHECK_GLOBALS(N)
     IMPLICIT NONE
     INTEGER,INTENT(IN)::N
@@ -194,6 +206,9 @@ SUBROUTINE CHECK_GLOBALS(N)
     IF (DIMENSIONA.EQ.2) THEN
         IF (NUM_DG_DOFS /= (IORDER+1) * (IORDER+2) / 2) WRITE(400+N,*) 'WARNING:NUM_DG_DOFS SHOULD BE', (IORDER+1) * (IORDER+2) / 2, 'BUT IS', NUM_DG_DOFS, 'IORDER:', IORDER
     END IF
+    
+    IF (IORDER == 2 .AND. QP_TRIANGLE /= 3) WRITE(400+N,*) "WARNING: QP_TRIANGLE SHOULD BE", 3, "BUT IS", QP_TRIANGLE
+    IF (IORDER == 2 .AND. QP_QUAD /= 4) WRITE(400+N,*) "WARNING: QP_QUAD SHOULD BE", 4, "BUT IS", QP_QUAD
 
 END SUBROUTINE CHECK_GLOBALS
 
@@ -229,9 +244,10 @@ SUBROUTINE RUN_ALL_TESTS(N)
     
     IF (TEST_BASIS_REC2D(N,ORDER,NUM_DOFS) == 0) WRITE(400+N,*) 'TEST_BASIS_REC2D passed'
     IF (TEST_DG_SOL(N,NUM_VARS,ORDER,NUM_DOFS) == 0) WRITE(400+N,*) 'TEST_DG_SOL passed'
-    IF (TEST_DG_RHS_VOL_INTEGRAL(N,ORDER,NUM_DOFS) == 0) WRITE(400+N,*) 'TEST_DG_RHS_VOL_INTEGRAL passed'
+    IF (TEST_DG_RHS_INTEGRAL(N,NUM_VARS,ORDER,NUM_DOFS) == 0) WRITE(400+N,*) 'TEST_DG_RHS_INTEGRAL passed'
     IF (TEST_QP_ARRAY(N) == 0) WRITE(400+N,*) 'TEST_QP_ARRAY passed'
     
+    CALL CHECK_GLOBALS(N)
     CALL PRINT_SOME_INFO(N)
     
 END SUBROUTINE
